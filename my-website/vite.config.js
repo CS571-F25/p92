41d67ae0e89560300c5dd6ee408a93473d474507
bcyang2 <bcyang2@wisc.edu>
2025-11-05 22:11:10 -0600

    Fix GitHub Pages: add .nojekyll to bypass Jekyll processing

--- my-website/vite.config.js
+++ my-website/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p92',
+  base: '/p92/',
   build: {
     outDir: 'docs'
   }
